703a30e973cee411b6030466eac1c2940ed8cb9b
amitnalwa <83637399+amitnalwa@users.noreply.github.com>
2023-07-12 09:04:30 +0530

    GuessGame.java

--- GuessGame.java
+++ GuessGame.java
@@ -3,21 +3,64 @@ import java.util.Scanner;
 
  class random1{
     int calculate = 0;
-    public void  guess(int a , int b){
+    public void hint(int a , int b){
+            
                 
+    int sum = a-b;
+    int d1;
+    int d2;
+    int d3;
+    int d4;
+    int d5;
+    int d6;
+        if (sum <= 15 && sum >= -10){
+            d6 = 96;
+            System.out.println("Your Guess is  : " + d6+"% Correct");
+        }
+           else if  (sum <= 7 && sum >= -10){
+            d1 = 90;
+            System.out.println("Your Guess is  : " + d1+"% Correct");
+        }
+        else if (sum <= 25 && sum >= -25){
+            d2 = 65;
+            System.out.println("Your Guess is  : " + d2+"% Correct");
+        }
+        
+         else if (sum <= 50 && sum >= -50){
+            d3 = 45;
+             System.out.println("Your Guess is  : " + d3+"% Correct");
+        }
+        
+           else if (sum <= 80 && sum >= -80){
+            d4 = 15;
+             System.out.println("Your Guess is  : " + d4+"% Correct");
+        }
+        else if (sum <= 95 && sum >= -95){
+            d5 = 4;
+             System.out.println("Your Guess is  : " + d5+"% Correct");
+        }
+        
+    }
+
+    public void  guess(int a , int b){
+    
+
+    // System.out.println(sum);        
+
         if (a < b){
                 System.out.println("Please Enter Small Value  : ");
                 calculate +=1;
+                hint(a,b);
         }
         else if (a>b) {
             System.out.println("Please Enter Large Value  : ");
                 calculate +=1;
-            
+            hint(a,b);
         } 
             else if (a==b) {
                 calculate +=1;
             System.out.println("Congratulation You Won And Your Attempt =  " + calculate);
-            
+            hint(a,b);
         } 
         
         
@@ -43,13 +86,15 @@ public static void main(String[] args){
  
 
     random1 rands = new random1();
-    System.out.println("Please Enter Int Value : ");
+    System.out.println("Please Enter Value : ");
+    System.out.println(number);
  
 
 while (true){
        
     int user = i.nextInt();
-      rands.guess(number , user);
+     rands.guess(number , user);
+
 
   if (number == user){
     break;
